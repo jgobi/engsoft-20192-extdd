31998c07a144d4d2ec9410a0a6dd535c7986cd7a
João Gabriel <joaogabrieloliveira9@gmail.com>
2019-11-03 17:22:44 -0300

    commit 2

--- src/Dollar.java
+++ src/Dollar.java
@@ -1,9 +1,11 @@
-class Dollar {
-    int amount = 10;
+class Dollar extends Money {
 
     Dollar(int amount) {
+        this.amount = amount;
     }
 
-    void times(int multiplier) {
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
     }
+
 }
